d8e495f4e7d02c3a704e49e2dd8bea0ce2a6d437
synergistics <kingjak678@gmail.com>
2016-07-02 22:17:52 +0000

    Remove unnecessary comments

--- Util/Graph.hs
+++ Util/Graph.hs
@@ -29,22 +29,3 @@ remove c g = filter (/= c) g
 
 removeMult :: (Eq n, Eq e) => [Context n e] -> Graph n e -> Graph n e 
 removeMult cs g = filter (\c -> not (elem c cs)) g
-
--- -- [e] requires flex contexts because its a concrete type
--- instance (Matching n, Matching [e]) => Matching (Context n e) where
---   match c1 c2 =  
---     match (ies c1) (ies c2) && 
---     match (node c1) (node c2) && 
---     match (oes c1) (oes c2)
-
-
--- -- nodeContext :: (Eq n, Eq e) => n -> Graph n e -> Maybe (Context n e) 
--- -- nodeContext n graph = L.find (\c -> n == node c) graph 
-
--- -- inContexts :: (Eq n, Eq e) => Context n e -> Graph n e -> Graph n e 
--- -- inContexts (Context ie  ) graph = filter (\(Context   oe) -> any (`elem` oe) ie) graph
-     
--- -- outContexts :: (Eq n, Eq e) => Context n e -> Graph n e -> Graph n e 
--- -- outContexts (Context   oe) graph = filter (\(Context ie  ) -> any (`elem` ie) oe) graph
-
-
