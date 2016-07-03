3c6d2ba00ca9f364afb138ecd932cf135daf9d96
synergistics <kingjak678@gmail.com>
2016-07-03 11:40:14 +0000

    Add distA move

--- Chemlambda/Moves.hs
+++ Chemlambda/Moves.hs
@@ -95,12 +95,13 @@ comb (matchOn (hasOf ro Arrow) -> Just someNode@(c G.:& g)) =
     newGraph
 comb g = g
 
-
+distL :: Move
 distL (matchOn ((nodeOf L)    <&&> 
                 (hasOf ro FO) <||>
                 (hasOf ro FOE)) -> Just lamNode@(c G.:& g)) = 
   let
     unusedEdges = [0..] \\ edges g
+    -- FO or FOE
     Just foNode = ro lamNode
 
     Just edgeA = miRef lamNode 
@@ -126,3 +127,37 @@ distL (matchOn ((nodeOf L)    <&&>
   in
     newGraph
 distL g = g
+
+
+distA :: Move
+distA (matchOn ((nodeOf A)    <&&>
+                (hasOf mo FO) <||>
+                (hasOf mo FOE)) -> Just appNode@(c G.:& g)) = 
+  let
+    unusedEdges = [0..] \\ edges g
+    -- FO or FOE
+    Just foNode = mo appNode
+
+    Just edgeA = liRef appNode
+    Just edgeB = riRef appNode
+    Just edgeC = moRef appNode
+    Just edgeD = loRef foNode
+    Just edgeE = roRef foNode
+    edgeK = unusedEdges !! 0
+    edgeL = unusedEdges !! 1
+    edgeI = unusedEdges !! 2
+    edgeJ = unusedEdges !! 3
+
+    
+    appCtx = ctx appNode
+    foCtx  = ctx foNode
+    
+    result = [ foe edgeA edgeI edgeJ
+             , foe edgeB edgeK edgeL 
+             , app edgeI edgeK edgeD
+             , app edgeJ edgeL edgeE
+             ]
+
+    newGraph = removeCtxs [appCtx, foCtx] g ++ result
+  in
+    newGraph
