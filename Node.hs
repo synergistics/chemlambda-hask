5a439e39d3280aadb2732a08c31cf74695eac0b3
synergistics <kingjak678@gmail.com>
2016-07-09 00:03:07 +0000

    Implement nodes using Ports

--- Node.hs
+++ Node.hs
@@ -1,69 +1,61 @@
 module Node where
 
+import qualified Data.List as L
 import Atom
-
+import Port
 
 data Node a = Node 
   { atom  :: Atom
-  , ports :: [a] }
-  deriving ( Show, Eq )
+  , ports :: [Port a] }
+  deriving ( Ord, Eq )
 
--- Smart constructors
+instance Show a => Show (Node a) where
+  show (Node a ps) = "Node " ++ show a ++ " " ++ show ps
+
+-- Constructors
 lam :: a -> a -> a -> Node a
-lam a b c = Node L [a,b,c]
+lam a b c = Node L [Mi a, Lo b, Ro c]
 
 fo :: a -> a -> a -> Node a
-fo a b c = Node FO [a,b,c] 
+fo a b c = Node FO [Mi a, Lo b, Ro c]
 
 foe :: a -> a -> a -> Node a
-foe a b c = Node FOE [a,b,c]
+foe a b c = Node FOE [Mi a, Lo b, Ro c]
 
 app :: a -> a -> a -> Node a
-app a b c = Node A [a,b,c]
+app a b c = Node A [Li a, Ri b, Mo c]
 
 fi :: a -> a -> a -> Node a
-fi a b c = Node FI [a,b,c]
+fi a b c = Node FI [Li a, Ri b, Mo c]
 
 arrow :: a -> a -> Node a
-arrow a b = Node ARROW [a,b]
+arrow a b = Node ARROW [Mi a, Mo b]
 
 frin :: a -> Node a
-frin a = Node FRIN [a] 
+frin a = Node FRIN [Mo a] 
 
 frout :: a -> Node a
-frout a = Node FROUT [a]
+frout a = Node FROUT [Mi a]
 
 t :: a -> Node a
-t a = Node T [a] 
+t a = Node T [Mi a] 
 
 -- Selectors
-liPort :: Node a -> Maybe a
-liPort n = if elem (atom n) [A,FI] 
-  then Just $ ports n !! 0 
-  else Nothing
+liPort :: Node a -> Maybe (Port a)
+liPort n = L.find isLi $ ports n 
 
-riPort :: Node a -> Maybe a
-riPort n = if elem (atom n) [A,FI] 
-  then Just $ ports n !! 1 
-  else Nothing
+riPort :: Node a -> Maybe (Port a)
+riPort n = L.find isRi $ ports n
 
-miPort :: Node a -> Maybe a
-miPort n = if elem (atom n) [L,FO,FOE,ARROW,FROUT,T] 
-  then Just $ ports n !! 0 
-  else Nothing
+miPort :: Node a -> Maybe (Port a)
+miPort n = L.find isMi $ ports n
 
-loPort :: Node a -> Maybe a
-loPort n = if elem (atom n) [L,FO,FOE] 
-  then Just $ ports n !! 1 
-  else Nothing
+loPort :: Node a -> Maybe (Port a)
+loPort n = L.find isLo $ ports n
 
-roPort :: Node a -> Maybe a
-roPort n = if elem (atom n) [L,FO,FOE] 
-  then Just $ ports n !! 2 
-  else Nothing
+roPort :: Node a -> Maybe (Port a)
+roPort n = L.find isRo $ ports n
 
-moPort :: Node a -> Maybe a
-moPort n = if elem (atom n) [A,FI,ARROW,FRIN] 
-  then Just $ last $ ports n 
-  else Nothing
+moPort :: Node a -> Maybe (Port a)
+moPort n = L.find isMo $ ports n
 
