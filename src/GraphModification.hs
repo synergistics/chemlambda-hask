5550648956ded168ea73292c746073c626b78d71
synergistics <kingjak678@gmail.com>
2016-07-17 23:26:32 +0000

    Rename GraphMod to GraphModification and likewise to similarly named functions

--- src/GraphModification.hs
+++ src/GraphModification.hs
@@ -1,14 +1,13 @@
-module GraphMod 
-  ( GraphMod(..)
+module GraphModification 
+  ( GraphModification(..)
   , PatternMove
-  , applyGraphMod
-  , graphMods -- rename this please
-  , allGraphMods
+  , applyGraphModification
+  , graphModifications -- rename this please
+  , allGraphModifications
   , addToGraph
   )
   where
 
-import qualified Data.List as L
 import Data.List ((\\))
 import qualified Data.Map as M
 import Data.Either
@@ -20,66 +19,66 @@ import Pattern
 import Moves
 
 
-data GraphMod a = GraphMod
+data GraphModification a = GraphModification
   { removedNodes :: [Node a]
-  , addedNodes   :: [GraphAddition a] }
+  , addedNodes   :: [NewNode a] }
   deriving ( Show )
 
 -- replace this with a tuple with two inner type sigs
-type PatternMove a = (Pattern a (Node a, Node a), (Node a, Node a) -> [GraphAddition a])
+type PatternMove a = (Pattern a (Node a, Node a), (Node a, Node a) -> [NewNode a])
 
-applyGraphMod :: (Eq a, Enum a) => GraphMod a -> Graph a -> Graph a
-applyGraphMod mod graph = 
+applyGraphModification :: (Eq a, Enum a) => GraphModification a -> Graph a -> Graph a
+applyGraphModification mod graph = 
   let
     withRemoved = Graph $ nodes graph \\ removedNodes mod
     withAdded   = addToGraph (addedNodes mod) withRemoved 
   in withAdded
 
-graphMods 
+graphModifications 
   :: (Eq a) 
   => PatternMove a
   -> Graph a 
-  -> [GraphMod a]
-graphMods (pattern, move) graph =  
+  -> [GraphModification a]
+graphModifications (pattern, move) graph =  
   let 
     matches       = nodesOnce $ match pattern graph
     removedNodes  = map (\(n1,n2) -> [n1, n2]) matches
     addedNodes    = map move matches
-    modifications = map (\(r,a) -> GraphMod r a) $ zipWith (,) removedNodes addedNodes
+    modifications = map (\(r,a) -> GraphModification r a) $ zipWith (,) removedNodes addedNodes
   in  
     modifications
      
 -- Each list has all of the patternMoves of the same priority level
 -- PatternMoves in the same list have the same priorty and act on the same Graph
 -- Later patterns cannot match on nodes used in previous patterns
-allGraphMods
+allGraphModifications
   :: Eq a
   => [[PatternMove a]]
   -> Graph a
-  -> [GraphMod a]
-allGraphMods patternMoveLists graph = concat $ snd $ 
+  -> [GraphModification a]
+allGraphModifications patternMoveLists graph = concat $ snd $ 
   foldl 
     (\(graph, mods) currentPatternMoves -> 
       let
-        mods'  = concatMap (\pm -> graphMods pm graph) currentPatternMoves
-        nodes' = nodes graph \\ concatMap removedNodes mods'
+        mods'  = currentPatternMoves >>= \pm -> graphModifications pm graph
+        nodes' = nodes graph \\ (mods' >>= removedNodes)
       in
-        (Graph $ nodes', mods' : mods))
-   (graph, [])
+        (Graph nodes', mods':mods))
+   (graph, []) -- accumulating the list of modifications
    patternMoveLists 
 
 
-addToGraph :: (Eq a, Enum a) => [GraphAddition a] -> Graph a -> Graph a
+addToGraph :: (Eq a, Enum a) => [NewNode a] -> Graph a -> Graph a
 addToGraph additions graph =
   let
     unused = unusedPortIds graph
 
-    reifyPort p = reify <$> p
+    reifyPort p = fmap reify p 
       where
-        reify (Left (New a)) = unused !! a 
-        reify (Right a)      = a
+        reify (NewId a)    = unused !! a 
+        reify (ActualId a) = a
 
-    toAddableNode (Node x ps) = Node x $ map reifyPort ps
+    toAddableNode (Node a ps) = Node a $ map reifyPort ps
 
     properAdditions = map toAddableNode additions
   in 
