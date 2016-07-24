5f601d0cad0241fc6446347f817bb1463a96d055
synergistics <kingjak678@gmail.com>
2016-07-24 23:37:17 +0000

    Add experimental folder, some benchmarking to be expanded later, and some cleanup

---
+++ src/Chemlambda/Experimental/Graph.hs
@@ -0,0 +1,44 @@
+module Chemlambda.Experimental.Graph where
+
+data Atom = L | FO | FOE | A | FI | ARROW | FRIN | FROUT | T deriving ( Show, Eq )
+data Node a = Node Atom [a] deriving ( Show, Eq )
+data Graph a = Graph { node :: a, nodes :: [Graph a] } deriving ( Show )
+
+class Comonad c where
+  extract :: c a -> a
+  unfold  :: c a -> c (c a)
+
+instance Comonad Graph where
+  extract (Graph n gs) = n
+
+  unfold g@(Graph n gs) = Graph g (map unfold gs)
+
+
+(!?) :: [a] -> Int -> Maybe a
+xs !? i =
+  if i > (length xs - 1) then Nothing else Just $ xs !! i
+
+atEdge :: Int -> Graph a -> Maybe (Graph a)
+atEdge i (Graph _ gs) = gs !? i
+
+
+connects :: Eq a => Node a -> Node a -> Bool
+connects n@(Node _ es) m@(Node _ ds) = any (`elem` ds) es && n /= m
+
+mkGraph :: Eq a => [Node a] -> Graph Atom 
+mkGraph ns =
+  let
+    n@(Node x es) = head ns
+    connectedNodes = filter (connects n) ns
+  in Graph x (map mkGraph $ map (\n -> n:filter (/= n) ns) connectedNodes)
+
+omega = 
+  [ Node L     [0,1,2]
+  , Node FO    [1,3,4]
+  , Node A     [3,4,0]
+  , Node L     [5,6,7]
+  , Node FO    [6,8,9]
+  , Node A     [8,9,5]
+  , Node A     [2,7,10]
+  , Node FROUT [10]
+  ]
---
+++ src/Chemlambda/Experimental/Reaction.hs
@@ -0,0 +1,70 @@
+module Chemlambda.Experimental.Reaction where
+
+import Data.List
+import Chemlambda.Core.Port
+import Chemlambda.Core.Node
+import Chemlambda.Core.Graph
+import Chemlambda.Core.Pattern
+import Chemlambda.Standard.Patterns
+import Chemlambda.Standard.Moves
+
+
+data Enzyme a = Enzyme
+  { pattern  :: Pattern [Node a] (Graph [Node a])
+  , move     :: Graph [Node a] -> Graph [Node (NewId a)]
+  , ePriority :: Int }
+
+data ReactionSite a = ReactionSite 
+  { site      :: Graph [Node a]
+  , reaction  :: Graph [Node a] -> Graph [Node (NewId a)]
+  , sPriority :: Int } 
+
+
+reactionSites :: Enzyme a -> Graph [Node a] -> [ReactionSite a] 
+reactionSites enzyme graph =
+  let matches = match (pattern enzyme) graph
+  in map (\graph' -> ReactionSite graph' (move enzyme) (ePriority enzyme)) matches
+
+
+betaEnzyme :: Eq a => Enzyme a
+betaEnzyme = Enzyme betaPattern betaMove 3 
+
+fanInEnzyme :: Eq a => Enzyme a
+fanInEnzyme = Enzyme fanInPattern fanInMove 3
+
+distAEnzyme :: Eq a => Enzyme a
+distAEnzyme = Enzyme distAPattern distAMove 4 
+
+distLEnzyme :: Eq a => Enzyme a
+distLEnzyme = Enzyme distLPattern distLMove 4 
+
+distFOEnzyme :: Eq a => Enzyme a
+distFOEnzyme = Enzyme distFOPattern distFOMove 5
+
+distFIEnzyme :: Eq a => Enzyme a
+distFIEnzyme = Enzyme distFIPattern distFIMove 4
+
+pruneAEnzyme :: Eq a => Enzyme a
+pruneAEnzyme = Enzyme pruneAPattern pruneAMove 2
+
+pruneFIEnzyme :: Eq a => Enzyme a
+pruneFIEnzyme = Enzyme pruneFIPattern pruneFIMove 2
+
+pruneLEnzyme :: Eq a => Enzyme a
+pruneLEnzyme = Enzyme pruneLPattern pruneLMove 2
+
+pruneFObEnzyme :: Eq a => Enzyme a
+pruneFObEnzyme = Enzyme pruneFObPattern pruneFObMove 1
+
+pruneFOcEnzyme :: Eq a => Enzyme a
+pruneFOcEnzyme = Enzyme pruneFOcPattern pruneFOcMove 0
+
+pruneFOEbEnzyme :: Eq a => Enzyme a
+pruneFOEbEnzyme = Enzyme pruneFOEbPattern pruneFOEbMove 1
+
+pruneFOEcEnzyme :: Eq a => Enzyme a
+pruneFOEcEnzyme = Enzyme pruneFOEcPattern pruneFOEcMove 0
+
+combEnzyme :: Eq a => Enzyme a
+combEnzyme = Enzyme combPattern combMove (-1)
+
