78c02c7da405e3f42b6ccef8ea4412c229569dc1
synergistics <kingjak678@gmail.com>
2016-08-07 05:25:56 +0000

    Add valence function to Core.Atom

--- chemlambda-core/Chemlambda/Core/Atom.hs
+++ chemlambda-core/Chemlambda/Core/Atom.hs
@@ -1,5 +1,7 @@
 module Chemlambda.Core.Atom 
-  ( Atom(..) )
+  ( Atom(..)
+  , valence 
+  )
   where
 
 -- A Chemlambda atom
@@ -14,3 +16,9 @@ data Atom
   | FROUT
   | T
   deriving ( Show, Eq, Ord )
+
+valence :: (Num a) => Atom -> a
+valence a | elem a [FRIN,FROUT,T]  = 1
+valence a | elem a [ARROW]         = 2
+valence a | elem a [L,FO,FOE,A,FI] = 3
+
